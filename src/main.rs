357d00efd50a0e430cd3adcab76c11f945bbe8e2
suap-dev <suap.dev@gmail.com>
2022-12-20 15:24:04 +0100

    Add prompt.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("I have a number in mind, try to guess it! I promiss I won't cheat.");
+    println!("Do you have your guess? Input it now:");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    println!("Your guess: {guess}");
 }
